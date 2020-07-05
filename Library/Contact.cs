68ebd45b34b5114c657e67158f4acc65c6582dd0
KamilP <61658872+Foscon@users.noreply.github.com>
2020-07-05 22:53:22 +0200

    Add files via upload

---
+++ Library/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Contact
+    {
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Phone Number: {PhoneNumber}";
+        }
+    }
+}
